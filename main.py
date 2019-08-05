3ba9916af590ea1b8118b82dd853b9ee2d14b864
Sihao Lu <sihao.lu@gmail.com>
2019-08-05 15:40:14 -0700

    clean up

--- main.py
+++ main.py
@@ -1,107 +1,69 @@
+from Objective import Objective
 from Medium import Medium
-from Photon import Photon
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
-
-def run_sim(mu_s, mu_a, n_i, n_e, g, num_photons):
-    # Start position
-    start_pos = np.array([0, 0, 0])
-
-    medium_shape = np.array([float('inf'), float('inf'), 500])
-
-    medium = Medium(medium_shape, mu_s, mu_a, n_i, n_e, g)
-
-    photon_positions = np.array([0, 0, 0])
-
-    for i in range(num_photons):
-        photon = Photon(start_pos, medium)
-
-        while photon.is_propagating and not photon.is_absorbed:
-            photon.propagate()
-        # print('Path length: %f' %photon.total_path)
-
-        if not photon.is_absorbed:
-            photon_positions = np.vstack((photon_positions, photon.path[-1]))
-
-    photon_positions = np.delete(photon_positions, 0, 0)
-
-    return photon_positions, photon
-
-
-def parallel_sim(mu_s, mu_a, n_i, n_e, g, num_photons):
-    result = [single_sim(mu_s, mu_a, n_i, n_e, g) for _ in range(num_photons)]
-
-    return result
-
-
-def single_sim(mu_s, mu_a, n_i, n_e, g):
-    # Start position
-    start_pos = np.array([0, 0, 750])
-    medium_shape = np.array([float('inf'), float('inf'), 1000])
-
-    medium = Medium(medium_shape, mu_s, mu_a, n_i, n_e, g)
-
-    photon = Photon(start_pos, medium)
-
-    while photon.is_propagating and not photon.is_absorbed:
-        photon.propagate()
-    # print('Path length: %f' %photon.total_path)
-
-    if not photon.is_absorbed:
-        return photon
-    else:
-        return None
-
+from itertools import compress
+import plotly
+import utils
 
 mpl.use('Qt5Agg')
-
 
 # Scattering coefficient
 # in um^-1
 mu_s = 0.01070
 
 # Absorption coefficient
-mu_a = 0.00001
+mu_a = 0.0002
 
 # Refractive indices
 # in tissue
 n_i = 1.4
 
-# in gel
-n_e = 1.34
+# in air
+n_e = 1
 
 # Anisotropy factor
 # in white matter
-g = 0.94
-photons = parallel_sim(mu_s, mu_a, n_i, n_e, g, 1000)
+g = .94
 
-positions = np.array([photon.path[-1] for photon in photons])
-print(positions.shape)
+sample_depth = 1000
+focus_depth = 170
+working_distance = 5940
+NA = 0.54
+fov = np.linspace(0, 0, 1)
 
-xx, yy = np.meshgrid(range(-100, 100), range(-100, 100), sparse=True)
-z_1 = xx * 0 + yy * 0
-z_2 = xx * 0 + yy * 0 + 260
+distance_to_sample = working_distance + focus_depth
 
-# Scatter plot exited photons
-fig_1 = plt.figure()
-ax_1 = plt.axes(projection='3d')
-ax_1.scatter(positions[:, 0], positions[:, 1], positions[:, 2], s=5)
-# ax.plot(photon_positions[:, 0], photon_positions[:, 1], photon_positions[:, 2])
+num_photons = 5000
 
-sns.jointplot(x=positions[:, 0], y=positions[:, 1])
-sns.jointplot(x=positions[:, 0], y=positions[:, 2])
+medium_shape = np.array([float('inf'), float('inf'), sample_depth])
+medium = Medium(medium_shape, mu_s, mu_a, n_i, n_e, g)
 
-fig_2 = plt.figure()
-ax_2 = plt.axes(projection='3d')
-ax_2.scatter(photons[0].path[:, 0], photons[0].path[:, 1], photons[0].path[:, 2], s=5)
-ax_2.plot(photons[0].path[:, 0], photons[0].path[:, 1], photons[0].path[:, 2])
+# xx, yy = np.meshgrid(range(-100, 100), range(-100, 100), sparse=True)
+# z_1 = xx * 0 + yy * 0
+# z_2 = xx * 0 + yy * 0 + 260
 
-# plt.figure()
-# theta_initials = np.array([theta for photon in photons for theta in photon.thetas])
-# sns.distplot(theta_initials)
+objective = Objective(NA, working_distance, sample_depth)
 
-plt.show()
+photons = utils.fov_sim(medium, fov, num_photons, focus_depth, omit_bottom=True)
+
+fig = utils.plot_photons(photons[0][0], objective, show_aperture=True)
+plotly.offline.plot(fig, filename='file_1.html')
+
+fig = utils.plot_photon_path(photons[0][0][0])
+plotly.offline.plot(fig, filename='file.html')
+
+acceptance_matrix = utils.calc_acceptance_matrix(photons, objective)
+fig = utils.plot_fov_heatmap(acceptance_matrix, fov)
+plotly.offline.plot(fig, filename='file2.html')
+
+# sns.jointplot(x=positions[:, 0], y=positions[:, 1])
+# sns.jointplot(x=positions[:, 0], y=positions[:, 2])
+#
+
+#
+# plt.show()
