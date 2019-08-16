672d5b642193639019c0da9ae56e65094823f6db
Sihao Lu <sihao.lu@gmail.com>
2019-08-16 10:50:48 -0700

    added acceptance rate vs depth plot

--- plots/depth_plots.py
+++ plots/depth_plots.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import plotly
 import utils
+import plotly.graph_objects as go
 
 # Scattering coefficient
 # in um^-1
@@ -26,18 +27,49 @@ sample_depth = 900
 working_distance = 5940
 NA = 0.54
 
-
 medium_shape = np.array([float('inf'), float('inf'), sample_depth])
 medium = Medium(medium_shape, mu_s, mu_a, n_i, n_e, g)
 
-num_photons = 5000
+num_photons = 7000
 
 start_positions = [
-    np.array([0, 0, 890]),
-    np.array([0, 0, 850]),
-    np.array([0, 0, 700]),
-    np.array([0, 0, 250]),
-    np.array([0, 0, 0]),
+    np.array([2500, 2500, 890]),
+    np.array([2500, 2500, 880]),
+    np.array([2500, 2500, 870]),
+    np.array([2500, 2500, 860]),
+    np.array([2500, 2500, 850]),
+    np.array([2500, 2500, 840]),
+    np.array([2500, 2500, 830]),
+    np.array([2500, 2500, 820]),
+    np.array([2500, 2500, 810]),
+    np.array([2500, 2500, 800]),
+    np.array([2500, 2500, 790]),
+    np.array([2500, 2500, 780]),
+    np.array([2500, 2500, 770]),
+    np.array([2500, 2500, 760]),
+    np.array([2500, 2500, 750]),
+    np.array([2500, 2500, 740]),
+    np.array([2500, 2500, 730]),
+    np.array([2500, 2500, 720]),
+    np.array([2500, 2500, 710]),
+    np.array([2500, 2500, 700]),
+    np.array([2500, 2500, 600]),
+    np.array([2500, 2500, 500]),
+    np.array([2500, 2500, 400]),
+    np.array([2500, 2500, 300]),
+    np.array([2500, 2500, 200]),
+    np.array([2500, 2500, 100]),
+    np.array([2500, 2500, 90]),
+    np.array([2500, 2500, 80]),
+    np.array([2500, 2500, 70]),
+    np.array([2500, 2500, 60]),
+    np.array([2500, 2500, 50]),
+    np.array([2500, 2500, 40]),
+    np.array([2500, 2500, 30]),
+    np.array([2500, 2500, 20]),
+    np.array([2500, 2500, 10]),
+    np.array([2500, 2500, 0]),
+
     np.array([2500, 2500, 890]),
     np.array([2500, 2500, 850]),
     np.array([2500, 2500, 700]),
@@ -45,12 +77,13 @@ start_positions = [
     np.array([2500, 2500, 0])
 ]
 
+ratios = []
 for start_pos in start_positions:
     distance_to_sample = working_distance + start_pos[2]
 
     objective = Objective(NA, working_distance, sample_depth)
 
-    photons = utils.multiple_sim(medium, start_pos, num_photons, omit_bottom=True)
+    photons = utils.multiple_sim(medium, start_pos, num_photons, omit_bottom=False)
 
     # Plot photon positions
     fig = utils.plot_photons(photons, objective)
@@ -73,3 +106,33 @@ for start_pos in start_positions:
         plotly.offline.plot(
             fig, filename='depth_plots/axial_path(%s,%s,%s).html' % (start_pos[0], start_pos[1], start_pos[2])
         )
+
+    ratios.append(utils.calc_acceptance_ratio(photons, objective))
+
+fig = go.Figure(
+    data=go.Scatter(
+        mode='lines',
+        x=[900 - pos[2] for pos in start_positions],
+        y=ratios
+    ),
+    layout=go.Layout(
+        title='Acceptance ratio with respect to depth of excitation',
+        xaxis=dict(
+            title='Depth from surface (um)',
+        ),
+        yaxis=dict(
+            title='Acceptance ratio (%%)'
+        )
+    )
+)
+
+try:
+    plotly.offline.plot(
+        fig, filename='depth_plots/acceptance_v_depth_off-axis.html'
+    )
+
+except FileNotFoundError:
+    os.mkdir('depth_plots')
+    plotly.offline.plot(
+        fig, filename='depth_plots/acceptance_v_depth_off-axis.html'
+    )
